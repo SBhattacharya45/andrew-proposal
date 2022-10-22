3958852b3f2e754bb966848a30526635f66ac0be
Soumyadeep <umyadeepb27@gmail.com>
2022-10-22 14:08:47 +0530

    more changes

--- js/chart.js
+++ js/chart.js
@@ -16,17 +16,29 @@ cleanGradient.addColorStop(0.5, 'rgba(34, 74,38, 0.5)');
 cleanGradient.addColorStop(1, 'rgba(34, 74,38, 0)');
 
 
-var tradPerYear = {
+var tradPerMonth = {
   '1 year': 200, 
   '5 year': 234, 
   '10 year': 285, 
   '15 year': 346, 
   '20 year': 421, 
   '25 year': 513, 
+  '26 year': 533, 
+  '30 year': 633, 
+};
+var cleanPerMonth = {
+  '1 year': 200, 
+  '5 year': 200, 
+  '10 year': 200, 
+  '15 year': 200, 
+  '20 year': 200, 
+  '25 year': 200, 
+  '26 year': 10, 
+  '30 year': 10, 
 };
 
 var tradData  = {
-    labels: ['1 year' ,'5 year','10 year', '15 year', '20 year','25 year'],
+    labels: ['1 year' ,'5 year','10 year', '15 year', '20 year','25 year', '26 year', '30 year'],
 datasets: [{
       label: 'Total paid per year',
       fill: true,
@@ -34,7 +46,7 @@ datasets: [{
       pointBackgroundColor: 'white',
       borderWidth: 1,
       borderColor: 'green',
-      data: [2400, 2400, 2400, 2400, 10,  10,]
+      data: [2400, 2400, 2400, 2400, 2400, 2400, 120,  120,]
     },{
       label: 'Total payment since the beginning',
       fill: true,
@@ -42,7 +54,7 @@ datasets: [{
       pointBackgroundColor: 'white',
       borderWidth: 1,
       borderColor: '#911215',
-      data: [2400, 12999,  28815 ,  48057 ,  71467 , 99950,]
+      data: [2400, 12999,  28815 ,  48057 ,  71467 , 99950, 106348, 134604]
     }]
 };
 
@@ -57,7 +69,7 @@ var cleanData  = {
     pointBackgroundColor: 'white',
     borderWidth: 1,
     borderColor: 'green',
-    data: [2400, 2400, 2400, 2400,  10, 10,]
+    data: [2400, 2400, 2400, 2400,  120, 120,]
   }]
 };
 
@@ -70,7 +82,8 @@ var options = {
   },
   scales: {
     y: {
-        suggestedMax: 100000
+        // suggestedMax: 100000,
+        type: 'logarithmic'
     }
   },
   plugins: {
@@ -82,10 +95,11 @@ var options = {
         label: function(tooltipItems) {
           console.log(tooltipItems);
           if(tooltipItems.datasetIndex === 1){
-            var multistringText = [`Monthly payment: ${tradPerYear[tooltipItems.label]}`, `Total paid since beginning: ${tooltipItems.formattedValue}`]
+            var multistringText = [`Monthly payment: ${tradPerMonth[tooltipItems.label]}`, `Total paid since beginning: ${tooltipItems.formattedValue}`]
             return  multistringText;
           } else {
-            return "Total paid per year: " + tooltipItems.formattedValue;
+            var multistringText = [`Monthly payment: ${cleanPerMonth[tooltipItems.label]}`, `Annual payment: ${tooltipItems.formattedValue}`]
+            return  multistringText;
           }
         }
       }
